59cd3c13d6c2eb7e217b2f1c505d6637b5117468
Julius Fan <jzfan@ucsc.edu>
2017-01-21 22:42:20 -0800

    Hui

--- app/src/main/java/com/soylentispeople/route_alarm/AlarmReceiver.java
+++ app/src/main/java/com/soylentispeople/route_alarm/AlarmReceiver.java
@@ -7,20 +7,55 @@ import android.content.Context;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
 
 
 /**
  * Created by krish98sai on 1/21/2017.
  */
 
-public class AlarmReceiver extends Activity {
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        //this will update the UI with message
+        AlarmActivity inst = AlarmActivity.instance();
+        inst.setAlarmText("Alarm! Wake up! Wake up!");
+
+        //this will sound the alarm tone
+        //this will sound the alarm once, if you wish to
+        //raise alarm in loop continuously then use MediaPlayer and setLooping(true)
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        //this will send a notification message
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
+/*public class AlarmReceiver extends WakefulBroadcastReceiver {
 
     private AlarmManager alarmManager;
     private PendingIntent alarmIntent;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+     public void onReceive(final Context context, Intent intent) {
+        AlarmActivity inst =
+        /*
         boolean repeat = getIntent().getBooleanExtra("repeat", false);
 
         alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
@@ -29,9 +64,9 @@ public class AlarmReceiver extends Activity {
         Calendar calendar = Calendar.getInstance();
 
         if(repeat) {
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, /*time of start*/ calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, /*time of start calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
         } else {
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, /*trigger millis*/ calendar.getTimeInMillis(), alarmIntent);
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, /*trigger millis calendar.getTimeInMillis(), alarmIntent);
         }
     }
-}
+}*/
---
+++ app/src/main/java/com/soylentispeople/route_alarm/AlarmService.java
@@ -0,0 +1,44 @@
+package com.soylentispeople.route_alarm;
+
+/**
+ * Created by thatq on 1/21/2017.
+ */
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class AlarmService extends IntentService {
+    private NotificationManager alarmNotificationManager;
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+        sendNotification("Wake Up! Wake Up!");
+    }
+
+    private void sendNotification(String msg) {
+        Log.d("AlarmService", "Preparing to send notification...: " + msg);
+        alarmNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, AlarmActivity.class), 0);
+
+        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.corner_white)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+
+        alamNotificationBuilder.setContentIntent(contentIntent);
+        alarmNotificationManager.notify(1, alamNotificationBuilder.build());
+        Log.d("AlarmService", "Notification sent.");
+    }
+}
